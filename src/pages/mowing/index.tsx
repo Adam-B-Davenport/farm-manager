242ec99aeb64d322010d8f4fa2deacbb0bcd518e
Adam Davenport <adam@galacticweapons.com>
2022-08-11 21:14:15 -0400

    Page tweaks

--- src/pages/mowing/index.tsx
+++ src/pages/mowing/index.tsx
@@ -1,6 +1,9 @@
 import type { Mowing, MowingLocation } from "@prisma/client";
 import type { NextPage } from "next";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
 import { NextRouter, useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { trpc } from "../../utils/trpc";
 
 type Mow = Mowing & { location: MowingLocation }
@@ -12,7 +15,7 @@ const MowingRow = ({ mowing, router }: { mowing: Mow, router: NextRouter }) => {
   }
 
   return (
-    <tr className="flex w-full hover:bg-green-100 border-b border-collapse border-slate-500 text-lg"
+    <tr className="flex w-full hover:bg-green-100 border-b border-collapse border-slate-500"
       onClick={() => edit(mowing.id)}>
       <td className="w-1/4">{mowing.location.name}</td>
       <td className="w-1/4">{mowing.direction}</td>
@@ -21,10 +24,9 @@ const MowingRow = ({ mowing, router }: { mowing: Mow, router: NextRouter }) => {
   )
 }
 
-const MowingTable = ({ mowings }: { mowings: Array<Mow> }) => {
-  const router = useRouter()
+const MowingTable = ({ mowings, router }: { mowings: Array<Mow>, router: NextRouter }) => {
   return (
-    <table className="text-left w-full max-w-md m-auto">
+    <table className="text-left w-full max-w-md m-auto text-2xl">
       <thead className="flex w-full">
         <tr className="flex w-full bg-green-500 text-neutral-100 px-2">
           <td className="w-1/4">Location</td>
@@ -32,7 +34,7 @@ const MowingTable = ({ mowings }: { mowings: Array<Mow> }) => {
           <td className="w-1/2">Date</td>
         </tr>
       </thead>
-      <tbody className="h-[50vh] flex flex-col overflow-y-auto w-full px-2">
+      <tbody className="h-[75vh] flex flex-col overflow-y-auto w-full px-2">
         {mowings.map(mow => <MowingRow key={mow.id} mowing={mow} router={router} />)}
       </tbody>
     </table>
@@ -40,23 +42,44 @@ const MowingTable = ({ mowings }: { mowings: Array<Mow> }) => {
 }
 
 const MowingPage: NextPage = () => {
-
+  const router = useRouter()
   const mowingQuery = trpc.useQuery(["mowing.getAll"])
-  if (!mowingQuery.data)
+
+  const [session, setSession] = useState<Session | null>(null)
+  useEffect(() => {
+    getSession()
+      .then(sess => {
+        if (!sess || sess === null)
+          router.push('/api/auth/signing')
+        else {
+          setSession(sess)
+        }
+      })
+      .catch(() => console.log("Failed to load session."))
+
+  }, [])
+
+  if (!mowingQuery.data || !session)
     return (
-      <>
-        <h1>Mowings</h1>
-        <p>Loading...</p>
-      </>
+      <div className="w-screen h-screen">
+        <p className="mar text-5xl mt-[50vh] text-center" >...</p>
+      </div>
     )
+  else if (session.user?.role !== 7) {
+    return (
+      <div className="w-screen h-screen">
+        <p className="mar text-5xl mt-[50vh] text-center" >Not Authorized </p>
+      </div>
+    )
+  }
   else {
     return (
       <>
-        <div className="container m-auto p-8 h-screen overflow-y-hidden">
+        <div className="container m-auto p-8 h-screen overflow-y-auto">
           <h1 className="text-center text-3xl">Mowing</h1>
-          <MowingTable mowings={mowingQuery.data} />
+          <MowingTable mowings={mowingQuery.data} router={router} />
           <div className="max-w-md m-auto">
-            <a href="/mowing/new" className="bg-green-500 rounded-lg px-8 py-2 text-neutral-50 text-2xl mt-8 float-right">+</a>
+            <a href="/mowing/new" className="bg-green-500 rounded-lg px-8 py-2 text-neutral-50 text-2xl mt-[-3rem] mr-[-5rem] float-right">+</a>
           </div>
         </div>
       </>
@@ -65,3 +88,4 @@ const MowingPage: NextPage = () => {
 }
 
 export default MowingPage
+
